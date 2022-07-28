bc20d1d353b361231d7f211bc56f5bf1a8abd62a
DisastrousFate <82060563+DisastrousFate@users.noreply.github.com>
2022-07-28 12:33:41 +0100

    Session post

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from math import fabs
-from urllib.robotparser import RobotFileParser
 import requests
 import json
 
@@ -14,3 +12,31 @@ with open("Settings.txt") as f: # Read settings from "Settings.txt"
         elif s[0].strip() == "Script_Id":
             SCRIPT_ID = s[1].strip()
 
+session = requests.session()
+session.cookies[".ROBLOSECURITY"] = ROBLOSECURITY
+req = session.post(
+    url="https://chat.roblox.com/v2/send-message",
+    data={
+        "message": "string",
+        "conversationId": 9873047824,
+        "decorators": [
+          "string"
+        ]
+    }
+)
+
+if "X-CSRF-Token" in req.headers:
+    session.headers["X-CSRF-Token"] = req.headers["X-CSRF-Token"]
+
+req = session.post(
+    url="https://chat.roblox.com/v2/send-message",
+    data={
+        "message": "Hello World!",
+        "conversationId": 9873047824,
+        "decorators": [
+          "string"
+        ]
+    }
+)
+
+print(json.loads(req.text))

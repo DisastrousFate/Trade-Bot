5a2525d4d805aefc3973e4c2b90edf609a68bddf
DisastrousFate <82060563+DisastrousFate@users.noreply.github.com>
2022-07-28 21:36:37 +0100

    a

--- main.py
+++ main.py
@@ -2,41 +2,37 @@ import requests
 import json
 
 ROBLOSECURITY = ""
-SCRIPT_ID = ""
 
 with open("Settings.txt") as f: # Read settings from "Settings.txt"
     for line in f:
         s = line.split("=")
         if s[0].strip() == ".ROBLOSECURITY":
             ROBLOSECURITY = s[1].strip()
-        elif s[0].strip() == "Script_Id":
-            SCRIPT_ID = s[1].strip()
 
 session = requests.session()
 session.cookies[".ROBLOSECURITY"] = ROBLOSECURITY
-req = session.post(
-    url="https://chat.roblox.com/v2/send-message",
-    data={
-        "message": "string",
-        "conversationId": 9873047824,
-        "decorators": [
-          "string"
-        ]
-    }
-)
 
-if "X-CSRF-Token" in req.headers:
-    session.headers["X-CSRF-Token"] = req.headers["X-CSRF-Token"]
+def rbx_request(method, url, **kwargs):
+    request = session.request(method, url, **kwargs)
+    method = method.lower()
+    if (method == "post") or (method == "put") or (method == "patch") or (method == "delete"):
+        if "X-CSRF-Token" in request.headers:
+            session.headers["X-CSRF-Token"] = request.headers["x-CSRF-Token"]
+            if request.status_code == 403: # request failed, send again
+                request = session.request(method, url, **kwargs)
+    return request
 
-req = session.post(
-    url="https://chat.roblox.com/v2/send-message",
-    data={
-        "message": "Hello World!",
-        "conversationId": 9873047824,
-        "decorators": [
-          "string"
-        ]
-    }
-)
+if __name__ == "__main__":
 
-print(json.loads(req.text))
+    print("Checking auth code..")
+    req = rbx_request("GET", "https://trades.roblox.com/v1/trades/Inbound?sortOrder=Asc&limit=10") # Check for incorrect auth code
+    if req.status_code != 200:
+        print(".ROBLOSECURITY Incorrect. Check Settings")
+        exit()
+    print("Correct! Starting trade bot..")
+    
+    players = rbx_request("GET", "https://groups.roblox.com/v1/groups/650266/roles/21783158/users?sortOrder=Desc&limit=100")
+    print(players.text)
+    
+    with open("text.txt", "w") as f:
+        f.write(players.text) 

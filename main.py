d304eef465bb60fbca4d70dfbe3b993bc3181a87
DisastrousFate <82060563+DisastrousFate@users.noreply.github.com>
2022-08-03 23:42:55 +0100

    searches through user inv to find 2nd item

--- main.py
+++ main.py
@@ -1,99 +1,84 @@
 import requests
 import json
 
-ROBLOSECURITY = ""
-USERID = 0
+def main():
 
-with open("Settings.txt") as f: # Read settings from "Settings.txt"
-    for line in f:
-        s = line.split("=")
-        if s[0].strip() == ".ROBLOSECURITY":
-            ROBLOSECURITY = s[1].strip()
-        if s[0].strip() == "UserId":
-            USERID = int(s[1].strip())
+    ROBLOSECURITY = ""
+    USERID = 0
 
-session = requests.session()
-session.cookies[".ROBLOSECURITY"] = ROBLOSECURITY
-session.headers["Content-Type"] = "application/json"
-
-def rbx_request(method, url, **kwargs):
-    request = session.request(method, url, **kwargs)
-    method = method.lower()
-    if (method == "post") or (method == "put") or (method == "patch") or (method == "delete"):
-        if "X-CSRF-Token" in request.headers:
-            session.headers["X-CSRF-Token"] = request.headers["x-CSRF-Token"]
-            if request.status_code == 403: # request failed, send again
-                request = session.request(method, url, **kwargs)
-    return request
-
-if __name__ == "__main__":
-
+    # Read settings from "Settings.txt"
     print("Checking auth code..")
-    req = rbx_request("GET", "https://trades.roblox.com/v1/trades/Inbound?sortOrder=Asc&limit=10") # Check for incorrect auth code
+    with open("Settings.txt") as f:
+        for line in f:
+            s = line.split("=")
+            if s[0].strip() == ".ROBLOSECURITY":
+                ROBLOSECURITY = s[1].strip()
+            if s[0].strip() == "UserId":
+                USERID = int(s[1].strip())
+
+    global session
+    session = requests.session()
+    session.cookies[".ROBLOSECURITY"] = ROBLOSECURITY
+    session.headers["Content-Type"] = "application/json"
+
+    # Check for incorrect auth code
+    req = rbx_request("GET", "https://trades.roblox.com/v1/trades/Inbound?sortOrder=Asc&limit=10")
     if req.status_code != 200:
         print(".ROBLOSECURITY Incorrect. Check Settings")
         exit()
+    print("Auth code correct!")
 
     req2 = session.post(url="https://auth.roblox.com/")
     if "X-CSRF-Token" in req2.headers:  # check if token is in response headers
         session.headers["X-CSRF-Token"] = req2.headers["X-CSRF-Token"]
 
-    print("Enter item id's of limiteds you want, each seperated by whitespace: ")       # Inputs
-    itemsWanted = input().split()
+    print("Enter asset id's of limiteds to offer, each seperated by whitespace: ")
+    assetOffers = ["6803423284", "6803400584"]
 
-    print("Enter item id's of limiteds to trade, each seperated by whitespace")
-    tradeItems = input().split()
+    print("Enter asset id's of limiteds to request, each seperated by whitespace: ")
+    assetRequests = ["398673908", "19027209"]
 
-    print("Correct! Starting trade bot..")
+    print("Starting trade bot..")
 
-    def switchPages(itemWanted):        # Switch inventory pages
-        pageNum, page, nextPageCursor = 0, None, None
-        if pageNum == 0:
-            page = json.loads(rbx_request("GET", "https://inventory.roblox.com/v2/assets/"+itemWanted+"/owners?sortOrder=Desc&limit=10").text)
-            pageNum =+ 1
-            nextPageCursor = page.get("nextPageCursor")
-        else:
-            page = json.loads(rbx_request("GET", "https://inventory.roblox.com/v2/assets/"+itemWanted+"/owners?sortOrder=Desc&limit=100&"+nextPageCursor).text)
-            pageNum =+ 1
-            nextPageCursor = page.get("nextPageCursor")
-        return pageNum, page, nextPageCursor
+    # Convert client asset id's to UAID
+    clientUAIDs = []
+    clientInv = json.loads(rbx_request("GET", "https://inventory.roblox.com/v1/users/"+str(USERID)+"/assets/collectibles?sortOrder=Desc&limit=10").text)
+    for assetoffer in assetOffers:
+        for asset in clientInv["data"]:
+            if asset["assetId"] == int(assetoffer):
+                clientUAIDs.append(asset["userAssetId"])
 
-    def sendTrade(UID, UAID, UAID2):                          # Send user trade. parameters are user to trade with, and user lgoged in
-        tradereq = session.post(url = "https://trades.roblox.com/v1/trades/send",
-            data = json.dumps({
-                "offers": [
-	                {
-			            "userId": UID,
-			            "userAssetIds": [UAID],
-			            "robux": 0,
-		            },
-		            {
-			            "userId": USERID,
-			            "userAssetIds": UAID2,
-			            "robux": 0,
-		            },
-	            ],
-            }),
-            headers = session.headers,
-            cookies = session.cookies)
-    
-    # Get UAID of item to trade
-    UAID_list = []
-
-    for item in tradeItems:
-        req = json.loads(rbx_request("GET", "https://inventory.roblox.com/v1/users/"+str(USERID)+"/assets/collectibles?sortOrder=Desc&limit=100").text)
-        for asset in req["data"]:
-            if asset["assetId"] == int(item):
-                UAID_list.insert(len(UAID_list) - 1, int(asset["userAssetId"]))
-    print(UAID_list)
-
-    for itemWanted in itemsWanted:
-        index = 0
-
-        pageNum, page, nextPageCursor = switchPages(itemWanted)
-        for user in page["data"]:
-            if user["owner"] == "null" or user["owner"] == None: # Check if user has premium
+    assetOwners, nextPage = assetOwner_request(assetRequests[0]) ## LOOP THROUGH PLAYERS WITH FIRST LIMITED
+    for user in assetOwners["data"]:        ## LOOP THROUGH USERS
+            # Check if user has premium
+            if user["owner"] == "null" or user["owner"] == None: 
                 continue
             
+            # Search through user inventory
+            userInv = json.loads(rbx_request("GET", "https://inventory.roblox.com/v1/users/"+str(user["owner"]["id"])+"/assets/collectibles?sortOrder=Desc&limit=10").text)
+            for assetRequest in assetRequests[1:]:
+                for userDict in userInv["data"]:
+                    if int(assetRequest) == userDict["assetId"]:
+                        print(user["owner"]["id"])
 
-#sendTrade(user["owner"]["id"], user["id"], UAID_list) # userId other player : your userId : items wanted 
+def rbx_request(method, url, **kwargs):
+
+        request = session.request(method, url, **kwargs) 
+        method = method.lower()
+        if (method == "post") or (method == "put") or (method == "patch") or (method == "delete"):
+            if "X-CSRF-Token" in request.headers:
+                session.headers["X-CSRF-Token"] = request.headers["x-CSRF-Token"]
+                if request.status_code == 403: # request failed, send again
+                    request = session.request(method, url, **kwargs)
+        return request
+
+def assetOwner_request(assetId, **page):
+    req = json.loads(rbx_request("GET", "https://inventory.roblox.com/v2/assets/"+str(assetId)+"/owners?sortOrder=Desc&limit=100").text)
+    nextPage = ""
+    if "nextPageCursor" in req:
+        nextPage = req["nextPageCursor"]
+    
+    return req, nextPage
+
+if __name__ == "__main__":
+    main()
